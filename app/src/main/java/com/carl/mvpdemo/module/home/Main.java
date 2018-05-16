2e0e0bd5ff32ecf988f32e04bf217037cfc603be
ruiqin.shen <jianesrq0724@outlook.com>
2018-05-16 15:52:04 +0800

    init

---
+++ app/src/main/java/com/carl/mvpdemo/module/home/Main.java
@@ -0,0 +1,23 @@
+package com.carl.mvpdemo.module.home;
+
+/**
+ * @author Carl
+ * @version 1.0
+ * @since 2018/5/16
+ */
+public class Main {
+    private String test;
+
+    public Main(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+}
---
+++ app/src/main/java/com/carl/mvpdemo/module/home/view/MainActivity.java
@@ -0,0 +1,79 @@
+package com.carl.mvpdemo.module.home.view;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.carl.mvpdemo.R;
+import com.carl.mvpdemo.module.home.Main;
+import com.carl.mvpdemo.pub.base.BaseActivity;
+import com.carl.mvpdemo.module.home.interfaces.MainI;
+import com.carl.mvpdemo.module.home.presenter.MainPresenter;
+import com.carl.mvpdemo.pub.utils.ToastUtils;
+
+/**
+ * @author Carl
+ * @version 1.0
+ * @since 2018/5/10
+ */
+public class MainActivity extends BaseActivity<MainI, MainPresenter> implements MainI {
+
+    private Button mButton;
+
+
+    @Override
+    protected void findView() {
+        mButton = findViewById(R.id.button);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+        mToolbarManager.hideBackIcon();
+        mToolbarManager.setToolbarTitle("MVP Demo");
+
+//        viewDataBinding.setMain(new Main("Test"));
+//        ActivityMainBinding activityMainBinding = viewDataBinding;
+    }
+
+    @Override
+    public void setOnInteractListener() {
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPresenter.testLogin();
+            }
+        });
+    }
+
+    @Override
+    public MainPresenter createPresenter() {
+        return new MainPresenter();
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void loginSuccess(String msg) {
+        ToastUtils.showShort(msg);
+    }
+
+    private long lastClickTime;
+
+    @Override
+    public void onBackPressed() {
+        long currentClickTime = System.currentTimeMillis();
+        if (currentClickTime - lastClickTime > 2000) {
+            ToastUtils.showShort("再按一次退出");
+            lastClickTime = currentClickTime;
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
